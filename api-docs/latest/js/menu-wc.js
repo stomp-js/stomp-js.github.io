d9d055179322f8e03d9cecda9133484de5df9e94
Deepak Kumar <deepak@kreatio.com>
2025-09-23 09:24:59 +0000

    Committed by Github action
    master: {
      "stompjs": "7.2.0",
      "rx-stomp": "2.2.0"
    }
    develop: {
      "stompjs": "7.2.0",
      "rx-stomp": "2.2.0"
    }

--- api-docs/latest/js/menu-wc.js
+++ api-docs/latest/js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">stompjs@7.2.0, rx-stomp@2.1.0</a>
+                    <a href="index.html" data-type="index-link">stompjs@7.2.0, rx-stomp@2.2.0</a>
                 </li>
 
                 <li class="divider"></li>
